1694d976c39b3e783a7f776d46ce2196bb5d5423
Pete Richardson <pete@peterichardson.com>
2022-04-19 14:20:46 -0700

    Migrate SerialPortSample code to class Serial in serial_connect target

--- spaceball-examples/serial_connect/main.cc
+++ spaceball-examples/serial_connect/main.cc
@@ -1,12 +1,21 @@
 #include <iostream>
 #include "serial.h"
 
+#define MAX_EVENT_SIZE 32
+
 int main(int argc, char*argv[]) {
     std::cout << "Hello, serial_connect!" << std::endl;
     
     auto ser = Serial("/dev/tty.usbserial-AJ03ACPV", 9600);
     ser.connect();
 
+    uint8_t event[MAX_EVENT_SIZE];
+    uint8_t *eventPtr = event;
+    while(true) {
+        auto length = ser.getEvent(eventPtr);
+        //std::cout << "length = " << length << std::endl;
+        ser.dumpEvent(event, length);
+    }
 
     ser.disconnect();
     std::cout << "Goodbye, serial_connect!" << std::endl;
