27f242bba1568f183e8406e79ec797d6012ad07f
Pete Richardson <pete@peterichardson.com>
2026-06-27 11:46:56 -0700

    refactor(mathgame): replace pthread with std::jthread and std::atomic
    Raw pthread_create/join and an unsynchronized static int are replaced
    with std::jthread (auto-joins on destruction) and std::atomic<int> to
    make the data race on answer well-defined. Also removes the unused
    WaitForAnswer function.

--- spaceball-examples/mathgame/main.cc
+++ spaceball-examples/mathgame/main.cc
@@ -1,16 +1,15 @@
 #include <iostream>
+#include <atomic>
+#include <thread>
 #include <stdlib.h>
-#include <unistd.h>
 #include <random>
 #include <chrono>
 
 #include "spaceball.h"
 
-static int answer = -1;
+static std::atomic<int> answer{-1};
 
-void *spaceball_monitor(void *arg) {
-    // A thread to monitor button input from the Spaceball and update
-    // a static variable (answer)
+void spaceball_monitor(std::atomic<int>& answer) {
     auto sb = Spaceball("/dev/tty.usbserial-AJ03ACPV");
     answer = -1;
     while (true) {
@@ -19,10 +18,7 @@ void *spaceball_monitor(void *arg) {
             break;
 
         if (auto* ke = std::get_if<KeyEvent>(&*event)) {
-            if (ke->pick) {
-                answer = 0;
-                break;
-            }
+            if (ke->pick) { answer = 0; break; }
             bool any = false;
             for (int i = 0; i < 8; i++) {
                 if (ke->buttons[i]) { answer = i + 1; any = true; }
@@ -30,47 +26,34 @@ void *spaceball_monitor(void *arg) {
             if (any) break;
         }
     }
-    return 0;
-}
-
-int WaitForAnswer(pthread_t &t) {
-    
-    return answer;
 }
 
 int main(void) {
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    std::mt19937 generator(seed);  // mt19937 is a standard mersenne_twister_engine
-  
+    std::mt19937 generator(seed);
+
     std::cout << "Welcome to the Math game!\n\n";
     std::cout << "Use the Spaceball buttons to answer the questions.\n";
     std::cout << "Press the Pick button (on the ball) to quit.\n\n";
 
-    int x,y;
+    int x, y;
     bool generate_new_question = true;
     while (true) {
-        pthread_t spaceball_thread;
-        int err = 0;
-
-        err = pthread_create(&spaceball_thread, NULL, spaceball_monitor, NULL);
-        if (err) {
-            std::cerr << "# Error: " << strerror(err) << " (" << stderr << ")\n";
-            exit(EXIT_FAILURE);
-        }
+        std::jthread spaceball_thread(spaceball_monitor, std::ref(answer));
 
         if (generate_new_question) {
-            x = (generator() % (4 - 1 + 1) ) + 1;
-            y = (generator() % (4 - 1 + 1) ) + 1;
+            x = (generator() % 4) + 1;
+            y = (generator() % 4) + 1;
         }
         std::cout << "What is " << x << " + " << y << "? " << std::flush;
-        
-        pthread_join(spaceball_thread, NULL);
-        
+
+        spaceball_thread.join();
+
         if (answer == 0) {
             std::cout << "\nGoodbye" << std::endl;
             break;
         }
-        
+
         if (answer == x + y) {
             std::cout << answer << " is correct!\n";
             generate_new_question = true;
@@ -80,5 +63,5 @@ int main(void) {
         }
     }
 
-    exit(EXIT_SUCCESS);
+    return EXIT_SUCCESS;
 }
